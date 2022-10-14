a3b92be8d0554a7ce9d525dee11523623e54cf97
Anton Ivanov <a.ivanov@magnus-tech.ru>
2022-10-14 18:34:35 +0300

    Initial commit

---
+++ middlewares/middleware.go
@@ -0,0 +1,24 @@
+package middlewares
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/ronnys-dev/golang_auth_service/services"
+)
+
+func Auth() gin.HandlerFunc {
+	return func(context *gin.Context) {
+		tokenString := context.GetHeader("Authorization")
+		if tokenString == "" {
+			context.JSON(401, gin.H{"error": "request does not contain an access token"})
+			context.Abort()
+			return
+		}
+		err := services.ValidateToken(tokenString)
+		if err != nil {
+			context.JSON(401, gin.H{"error": err.Error()})
+			context.Abort()
+			return
+		}
+		context.Next()
+	}
+}
